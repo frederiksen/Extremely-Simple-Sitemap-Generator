01501a67d174d98e91869bc4ace52b3880a6df1d
Morten Frederiksen <mrtn.frederiksen@gmail.com>
2015-10-21 17:08:18 +0200

    Implementation

--- ExtremelySimpleSitemapGenerator/ExtremelySimpleSitemapGenerator/SitemapGenerator.cs
+++ ExtremelySimpleSitemapGenerator/ExtremelySimpleSitemapGenerator/SitemapGenerator.cs
@@ -1,12 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Xml.Linq;
 
 namespace ExtremelySimpleSitemapGenerator
 {
-  public class SitemapGenerator
+  /// <summary>
+  /// Documentation:
+  /// https://github.com/frederiksen/ExtremelySimpleSitemapGenerator
+  /// </summary>
+  public abstract class SitemapGenerator
   {
+    public static XDocument Sitemap(List<SitemapItem> items)
+    {
+      //https://support.google.com/webmasters/answer/2620865?hl=en
+
+      XNamespace ns = "http://www.sitemaps.org/schemas/sitemap/0.9";
+      XNamespace xhtml = "http://www.w3.org/1999/xhtml";
+
+      var xmlDoc = new XDocument(new XDeclaration("1.0", "utf-8", null));
+
+      var root = new XElement(
+        ns + "urlset",
+        new XAttribute("xmlns", ns.NamespaceName),
+        new XAttribute(XNamespace.Xmlns + "xhtml", xhtml)
+        );
+
+      foreach (var item in items)
+      {
+        var node = new XElement(ns + "url");
+        var nodeloc = new XElement(ns + "loc", item.Url);
+        node.Add(nodeloc);
+
+        // alternate
+        foreach (KeyValuePair<string, string> entry in item.Alternate)
+        {
+          var a = new XElement(xhtml + "link",
+               new XAttribute("rel", "alternate"),
+               new XAttribute("hreflang", entry.Key),
+               new XAttribute("href", entry.Value)
+          );
+          node.Add(a);
+        }
+
+        // add to root
+        root.Add(node);
+      }
+
+      xmlDoc.Add(root);
+
+      return xmlDoc;
+    }
   }
 }
--- ExtremelySimpleSitemapGenerator/ExtremelySimpleSitemapGenerator/SitemapItem.cs
+++ ExtremelySimpleSitemapGenerator/ExtremelySimpleSitemapGenerator/SitemapItem.cs
@@ -1,12 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace ExtremelySimpleSitemapGenerator
 {
+  /// <summary>
+  /// Documentation:
+  /// https://github.com/frederiksen/ExtremelySimpleSitemapGenerator
+  /// </summary>
   public class SitemapItem
   {
+    public SitemapItem(string url, Dictionary<string, string> alternate)
+    {
+      Url = url;
+      Alternate = alternate;
+    }
+
+    public string Url { get; private set; }
+
+    public Dictionary<string, string> Alternate { get; private set; }
   }
 }
